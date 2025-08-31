8386afde97b7315b7f1f13ca48a99f3ae4369abb
中尾司 <tsukaz4298349@gmail.com>
2025-08-31 17:50:50 +0900

    Initial Commit

---
+++ Myjanken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Myjanken
+//
+//  Created by 中尾司 on 2025/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Myjanken/MyjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjankenApp.swift
+//  Myjanken
+//
+//  Created by 中尾司 on 2025/08/31.
+//
+
+import SwiftUI
+
+@main
+struct MyjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
